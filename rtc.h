1bd453d3cf54ebab8849d0f277886a998bb024d0
Julien Castelain <julien.castelain@liferay.com>
2018-11-18 14:28:42 +0100

    Add operators and update tests

--- rtc.h
+++ rtc.h
@@ -14,7 +14,33 @@ public:
 	tuple(float x, float y, float z, float w) :
 		x(x), y(y), z(z), w(w) {}
 
+	inline bool is_point() {
+		return w == 1.0;
+	}
 
+	inline bool is_vector() {
+		return w == 0.0;
+	}
+
+	tuple operator+(tuple a) {
+		x += a.x;
+		y += a.y;
+		z += a.z;
+		w += a.w;
+		return *this;
+	}
+
+	tuple operator-(tuple a) {
+		x -= a.x;
+		y -= a.y;
+		z -= a.z;
+		w -= a.w;
+		return *this;
+	}
+
+	bool operator==(const tuple& a) const {
+		return (x == a.x && y == a.y && z == a.z && w == a.w);
+	}
 };
 
 tuple point(float x, float y, float z) {
@@ -25,6 +51,9 @@ tuple vector(float x, float y, float z) {
 	return tuple(x, y, z, 0.0);
 }
 
+
 bool equal(float a, float b) {
 	return (abs(a - b) < EPSILON);
 }
+
+
--- test.cpp
+++ test.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
@@ -13,15 +11,53 @@ TEST_CASE("tuple should return a tuple instance") {
 	REQUIRE(t.z == 1);
 }
 
-TEST_CASE("point should return a tuple with w = 1.0") {
-	tuple p = point(-3, 2, 1);
+TEST_CASE("point should return a tuple with w=1.0") {
+	tuple t = point(-3, 2, 1);
 
-	REQUIRE(p.w == 1.0);
+	REQUIRE(t.w == 1.0);
+	REQUIRE(equal(t.w, 1.0));
+	REQUIRE(t.is_point());
 }
 
-TEST_CASE("vector should return a tuple with w = 0.0") {
-	tuple v = vector(-3, 2, 1);
+TEST_CASE("vector should return a tuple with w=0.0") {
+	tuple t = vector(-3, 2, 1);
 
-	REQUIRE(v.w == 0.0);
-	REQUIRE(equal(v.w, 0.0));
+	REQUIRE(t.w == 0.0);
+	REQUIRE(equal(t.w, 0.0));
+	REQUIRE(t.is_vector());
+}
+
+TEST_CASE("tuples can be added") {
+	tuple a1 = tuple(3, -2, 5, 1);
+	tuple a2 = tuple(-2, 3, 1, 0);
+
+	tuple a3 = a1 + a2;
+
+	REQUIRE(a3.x == 1);
+	REQUIRE(a3.y == 1);
+	REQUIRE(a3.z == 6);
+	REQUIRE(a3.w == 1);
+}
+
+TEST_CASE("tuples can be subtracted") {
+	tuple a1 = point(3, 2, 1);
+	tuple a2 = point(5, 6, 7);
+	tuple a3 = a1 - a2;
+	tuple a4 = vector(-2, -4, -6);
+
+	REQUIRE(a3.x == a4.x);
+	REQUIRE(a3.y == a4.y);
+	REQUIRE(a3.z == a4.z);
+	REQUIRE(a3.w == a4.w);
+	REQUIRE(a3 == a4);
+}
+
+TEST_CASE("substracting a vector from a point") {
+	tuple p = point(3, 2, 1);
+	tuple v = vector(5, 6, 7);
+
+	tuple a1 = p - v;
+	tuple a2 = point(-2, -4, -6);
+
+	REQUIRE(a1 == a2);
 }
