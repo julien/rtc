1667378462f6b4bca3ffa82683357526d855d02d
Julien Castelain <julien.castelain@liferay.com>
2018-11-23 20:13:36 +0100

    Capitalization

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 
 #include "rtc.h"
 
-TEST_CASE("tuple should return a tuple instance") {
+TEST_CASE("Tuple should return a tuple instance") {
 	tuple t(-3, 2, 1, 1);
 
 	REQUIRE(t.x == -3);
@@ -11,7 +11,7 @@ TEST_CASE("tuple should return a tuple instance") {
 	REQUIRE(t.z == 1);
 }
 
-TEST_CASE("point should return a tuple with w=1.0") {
+TEST_CASE("Point should return a tuple with w=1.0") {
 	tuple t = point(-3, 2, 1);
 
 	REQUIRE(t.w == 1.0);
@@ -19,7 +19,7 @@ TEST_CASE("point should return a tuple with w=1.0") {
 	REQUIRE(t.is_point());
 }
 
-TEST_CASE("vector should return a tuple with w=0.0") {
+TEST_CASE("Vector should return a tuple with w=0.0") {
 	tuple t = vector(-3, 2, 1);
 
 	REQUIRE(t.w == 0.0);
@@ -27,7 +27,7 @@ TEST_CASE("vector should return a tuple with w=0.0") {
 	REQUIRE(t.is_vector());
 }
 
-TEST_CASE("tuples can be added") {
+TEST_CASE("Tuples can be added") {
 	tuple a1 = tuple(3, -2, 5, 1);
 	tuple a2 = tuple(-2, 3, 1, 0);
 
@@ -39,7 +39,7 @@ TEST_CASE("tuples can be added") {
 	REQUIRE(a3.w == 1);
 }
 
-TEST_CASE("tuples can be subtracted") {
+TEST_CASE("Tuples can be subtracted") {
 	tuple a1 = point(3, 2, 1);
 	tuple a2 = point(5, 6, 7);
 	tuple a3 = a1 - a2;
@@ -52,7 +52,7 @@ TEST_CASE("tuples can be subtracted") {
 	REQUIRE(a3 == a4);
 }
 
-TEST_CASE("substracting a vector from a point") {
+TEST_CASE("Substracting a vector from a point") {
 	tuple p = point(3, 2, 1);
 	tuple v = vector(5, 6, 7);
 
@@ -62,71 +62,72 @@ TEST_CASE("substracting a vector from a point") {
 	REQUIRE(a1 == a2);
 }
 
-TEST_CASE("subtracting two vectors") {
+TEST_CASE("Subtracting two vectors") {
 	tuple v1 = vector(3, 2, 1);
 	tuple v2 = vector(5, 6, 7);
 
 	REQUIRE(v1 - v2 == vector(-2, -4, -6));
 }
 
-TEST_CASE("subtracting a vector from the zero vector") {
+TEST_CASE("Subtracting a vector from the zero vector") {
 	tuple zero = vector(0, 0, 0);
 	tuple v = vector(1, -2, 3);
 
 	REQUIRE(zero - v == vector(-1, 2, -3));
 }
 
-TEST_CASE("negating a tuple") {
+TEST_CASE("Negating a tuple") {
 	tuple a = tuple(1, -2, 3, -4);
 
 	REQUIRE(-a == tuple(-1, 2, -3, 4));
 }
 
-TEST_CASE("multiplying a tuple by a scalar") {
+TEST_CASE("Multiplying a tuple by a scalar") {
 	tuple a = tuple(1, -2, 3, -4);
 
 	REQUIRE(a * 3.5 == tuple(3.5, -7, 10.5, -14));
 }
 
-TEST_CASE("multiplying a tuple by a fraction") {
+TEST_CASE("Multiplying a tuple by a fraction") {
 	tuple a = tuple(1, -2, 3, -4);
 
 	REQUIRE(a * 0.5 == tuple(0.5, -1, 1.5, -2));
 }
 
 
-TEST_CASE("dividing a tuple by a scalar") {
+TEST_CASE("Dividing a tuple by a scalar") {
 	tuple a = tuple(1, -2, 3, -4);
 
 	REQUIRE(a / 2 == tuple(0.5, -1, 1.5, -2));
 }
 
-TEST_CASE("computing the magnitude of vector(1, 0, 0)") {
+TEST_CASE("Computing the magnitude of vector(1, 0, 0)") {
 	tuple v = vector(1, 0, 0);
 
 	REQUIRE(magnitude(v) == 1);
 }
 
-TEST_CASE("computing the magnitude of vector(0, 1, 0)") {
+TEST_CASE("Computing the magnitude of vector(0, 1, 0)") {
 	tuple v = vector(0, 1, 0);
 
 	REQUIRE(magnitude(v) == 1);
 }
 
-TEST_CASE("computing the magnitude of vector(0, 0, 1)") {
+TEST_CASE("Computing the magnitude of vector(0, 0, 1)") {
 	tuple v = vector(0, 0, 1);
 
 	REQUIRE(magnitude(v) == 1);
 }
 
-TEST_CASE("computing the magnitude of vector(1, 2, 3)") {
+TEST_CASE("Computing the magnitude of vector(1, 2, 3)") {
 	tuple v = vector(1, 2, 3);
 
 	REQUIRE(equal(magnitude(v), sqrt(14)));
 }
 
-TEST_CASE("computing the magnitude of vector(-1, -2, -3)") {
+TEST_CASE("Computing the magnitude of vector(-1, -2, -3)") {
 	tuple v = vector(-1, -2, -3);
 
 	REQUIRE(equal(magnitude(v), sqrt(14)));
 }
+
